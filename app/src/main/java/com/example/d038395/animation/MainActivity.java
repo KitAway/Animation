3948af86d8d71dcd79abae039248b255c95b0173
Truth <d038395@denethor.cdsdom.polito.it>
2015-07-14 18:27:23 +0200

    add tts, add animation, serial execution of tasks

--- app/src/main/java/com/example/d038395/animation/MainActivity.java
+++ app/src/main/java/com/example/d038395/animation/MainActivity.java
@@ -1,35 +1,54 @@
 package com.example.d038395.animation;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
 
     static File filepath;
     VideoView videoView;
-    String[] snowWiteList = new String[]{
-            "snow white",
-            "What's your name?",
-            "Would you like tell me the story about Snow White?",
-            "Excellent, thank you."
-    };
+    MyUtils.TTs tts;
+    ArrayList<String> snowWhiteList = new ArrayList<>();
+    String thanks="Excellent, thank you.";
+
+    final int mMain=0;
+    final int mTTs=1;
+    final int mRecord=2;
+    final boolean[] boolTask = new boolean[]{true,true,true};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        snowWhiteList.add("snow white");
+        snowWhiteList.add("What's your name?");
+        //snowWhiteList.add("Would you like tell me the story about Snow White?");
+
         filepath=new File(Environment.getExternalStorageDirectory().getPath()+File.separator+"Animation");
-        MyUtil.playVideo(this);
+        if (!filepath.isDirectory())
+            filepath.mkdirs();
+
+        MyUtils.playVideo(this);
 
         Uri uri =Uri.parse("android.resource://" + getPackageName() +'/'+ R.raw.animation_speaking);
         videoView = (VideoView) findViewById(R.id.video_hint);
@@ -40,6 +59,8 @@ public class MainActivity extends ActionBarActivity {
                 mp.start();
             }
         });
+
+        tts = new MyUtils.TTs(this);
 
     }
 
@@ -63,24 +84,141 @@ public class MainActivity extends ActionBarActivity {
         }
 
         if (id== R.id.action_start) {
-            startAct();
+            startAct(snowWhiteList);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void startAct(){
-        String topic=snowWiteList[0];
-        int len=snowWiteList.length;
-        for (int i=1;i<len-1;i++){
-            recording(topic,snowWiteList[i]);
+    private class MyParas{
+        String topic;
+        String question;
+        Iterator iterator;
+
+        public MyParas(String topic, String question, Iterator iterator) {
+            this.topic = topic;
+            this.question = question;
+            this.iterator = iterator;
+        }
+    }
+
+    private class taskTTs extends AsyncTask<MyParas,Void,MyParas> {
+
+        String topic;
+        String question;
+
+        @Override
+        protected MyParas doInBackground(MyParas...params) {
+            topic=params[0].topic;
+            question=params[0].question;
+            tts.SpeakText(question,question);
+            videoView.start();
+            boolTask[mTTs]=true;
+            tts.getTTs().setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onStart(String utteranceId) {
+
+                }
+
+                @Override
+                public void onDone(String utteranceId) {
+                    boolTask[mTTs] = false;
+                    videoView.stopPlayback();
+                }
+
+                @Override
+                public void onError(String utteranceId) {
+                    boolTask[mTTs] = false;
+                    videoView.stopPlayback();
+                }
+            });
+            while(boolTask[mTTs]){
+                try{
+                    Thread.sleep(100);
+                } catch (InterruptedException e ){
+                    e.printStackTrace();
+                }
+            }
+            return params[0];
         }
 
-        videoView.start();
+
+        @Override
+        protected void onPostExecute(MyParas params) {
+            new asyncRecording().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR,params);
+        }
+    }
+    public void startAct(ArrayList<?> arrayList){
+
+        Iterator<?> iterator = arrayList.iterator();
+
+
+        final String topic=iterator.next().toString();
+
+        if (iterator.hasNext())
+            actNext(topic,iterator);
     }
 
-    private void recording(String topic, String question){
+    public void actNext(String topic, Iterator<?> iterator){
+            String question=iterator.next().toString();
+            new taskTTs().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR,
+                    new MyParas(topic,question,iterator));
+    }
+
+    private class asyncRecording extends AsyncTask<MyParas, Void ,Void> {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        AlertDialog dialog;
+        MediaRecorder mRecorder = new MediaRecorder();
+        @Override
+        protected Void doInBackground(MyParas... params) {
+            final UUID uuid = UUID.randomUUID();
+            final String topic = params[0].topic;
+            final String question = params[0].question;
+            final Iterator iterator=params[0].iterator;
+
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            String audioPath = filepath + File.separator + uuid;
+            mRecorder.setOutputFile(audioPath);
+
+            mRecorder.setAudioChannels(1);
+            mRecorder.setAudioSamplingRate(44100);
+            mRecorder.setAudioEncodingBitRate(44100);
+
+            builder.setMessage("Recording")
+                    .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            mRecorder.stop();
+                            mRecorder.release();
+                            new DataStorage(uuid.toString(), question, topic).put();
+                            boolTask[mRecord] = false;
+                            boolTask[mMain]=false;
+                            if (iterator.hasNext())
+                                actNext(topic,iterator);
+                        }
+                    });
+            try {
+                mRecorder.prepare();
+                mRecorder.start();
+                boolTask[mRecord] = true;
+            } catch ( IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            dialog=builder.create();
+            dialog.show();
+        }
 
     }
+
+
 }
--- app/src/main/java/com/example/d038395/animation/MyUtils.java
+++ app/src/main/java/com/example/d038395/animation/MyUtils.java
@@ -2,16 +2,21 @@ package com.example.d038395.animation;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import java.util.Locale;
 
 
 /**
  * Created by d038395 on 2015-07-13.
  */
-public class MyUtil {
+public class MyUtils {
 
     static void  playVideo(Activity activity){
         final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
@@ -28,5 +33,29 @@ public class MyUtil {
         });
         videoView.start();
         dialog.show();
+    }
+
+    static class TTs{
+        Context context;
+        TextToSpeech textToSpeech;
+        public TTs(final Context context) {
+            this.context = context;
+            textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    if (status == TextToSpeech.SUCCESS) {
+                        Toast.makeText(context, "TTs initialized successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        public void SpeakText(String text, String utteranceId ) {
+            textToSpeech.setLanguage(Locale.US);
+            textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, null,utteranceId);
+        }
+
+        public TextToSpeech getTTs(){
+            return textToSpeech;
+        }
     }
 }
